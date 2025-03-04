c869dfac352f576803b4197c2874d56bca1c1b5c
Jasmin Patel <jasmin.patel2110@gmail.com>
2025-03-05 02:08:38 +0530

    edit and delete task done, some design improvments

--- Kanban board/script.js
+++ Kanban board/script.js
@@ -1,53 +1,98 @@
-let taskTemplate = `<p draggable="true" class="board-item"></p>`;
 const addClassBtn = document.querySelector(".add-class-btn");
-const todoBoardItems = document.querySelector("#todo-board > .board-items");
+const todoBoardItems = document.querySelector("#todo-board > .task-list");
 let taskList = document.querySelectorAll(".board-item");
-const dropBoardList = document.querySelectorAll(".board-items");
+const dropBoardList = document.querySelectorAll(".task-list");
 
 let draggedElement = null;
 let dropBoard = null;
 
+// add task button
 addClassBtn.addEventListener("click", (e) => {
-  // <p draggable="true" class="board-item"></p>
-  const innerText = prompt("Enter the task.");
+  const taskText = prompt("Enter the task.");
 
-  if (!innerText) return;
+  if (!taskText) return;
 
-  todoBoardItems.append(createTaskElement(innerText));
+  todoBoardItems.append(createTaskElement(taskText));
 });
 
-taskList.forEach((val, key) => {
-  addDragEvents(val);
-});
-
-dropBoardList.forEach((val, key) => {
-  val.addEventListener("dragover", (e) => {
-    if (e.target.className === "board-items") {
-      dropBoard = e.target;
-    }
-  });
-});
-
-function createTaskElement(innerText) {
+// create task element function
+function createTaskElement(taskText) {
   const task = document.createElement("p");
   task.className = "board-item";
   task.setAttribute("draggable", "true");
-  task.innerHTML = innerText;
+  task.innerHTML = `<span class="task-text">${taskText}</span>
+            <span class="edit-task fa-solid fa-pen-to-square"> </span>
+            <span class="delete-task fa-solid fa-trash"> </span>`;
 
   addDragEvents(task);
+  addEditEvent(task.children[1]);
+  addDeleteEvent(task.children[2]);
 
   return task;
 }
 
+// applying drag events to every task.
+taskList.forEach((val, key) => {
+  addDragEvents(val);
+});
+
+// applying drop events to every board.
+dropBoardList.forEach((val, key) => {
+  addDropEvent(val);
+});
+
+// Adds a dragstart and dragend event listeners to tasks to select and append a task to the selected drop board.
 function addDragEvents(element) {
   element.addEventListener("dragstart", (e) => {
     draggedElement = e.target;
     draggedElement.classList.add("dragging");
+    draggedElement.style.cursor = "grabbing";
   });
 
   element.addEventListener("dragend", (e) => {
-    dropBoard.appendChild(draggedElement);
-    draggedElement.classList.remove("dragging");
-    draggedElement = null;
+    try {
+      draggedElement.classList.remove("dragging");
+      draggedElement.style.cursor = "grab";
+      dropBoard.appendChild(draggedElement);
+    } catch (e) {
+    } finally {
+      draggedElement = null;
+      dropBoard = null;
+    }
+  });
+}
+
+// Adds a dragover event listener to select the drop board
+function addDropEvent(board) {
+  board.addEventListener("dragover", (e) => {
+    if (e.target.classList.contains("task-list")) {
+      dropBoard = e.target;
+    }
+  });
+}
+
+// edit task
+const editTaskBtn = document.querySelectorAll(".edit-task");
+editTaskBtn.forEach(addEditEvent);
+
+// delete task
+const deleteTaskBtnList = document.querySelectorAll(".delete-task");
+deleteTaskBtnList.forEach(addDeleteEvent);
+
+// Adds a click event listener to a button to delete task.
+function addDeleteEvent(btn) {
+  btn.addEventListener("click", function (e) {
+    this.parentElement.remove();
+  });
+}
+
+// Adds a click event listener to a button to edit task.
+function addEditEvent(btn) {
+  btn.addEventListener("click", function (e) {
+    const taskText = prompt("Enter the task.");
+
+    if (!taskText) return;
+
+    this.parentElement.children[0].innerHTML = taskText;
   });
 }
