6732aeb458e4c48808e908ac013c9e5dbd60866d
Jasmin Patel <jasmin.patel2110@gmail.com>
2025-03-04 14:02:24 +0530

    drag and drop functionality done, but not for newly added tasks

--- Kanban board/script.js
+++ Kanban board/script.js
@@ -1,9 +1,19 @@
 let taskTemplate = `<p draggable="true" class="board-item"></p>`;
 const addClassBtn = document.querySelector(".add-class-btn");
 const todoBoardItems = document.querySelector("#todo-board > .board-items");
+let taskList = document.querySelectorAll(".board-item");
+const dropBoardList = document.querySelectorAll(".board-items");
 
-addClassBtn.addEventListener("click", () => {
+let draggedElement = null;
+let dropBoard = null;
+
+// console.log(typeof taskList[0]);
+// console.log(dropBoardList);
+
+addClassBtn.addEventListener("click", (e) => {
   // <p draggable="true" class="board-item"></p>
+
+  console.log(e);
 
   const taskString = prompt("Enter the task.");
 
@@ -15,4 +25,31 @@ addClassBtn.addEventListener("click", () => {
   task.innerHTML = taskString;
 
   todoBoardItems.append(task);
+
+  // taskList = document.querySelectorAll(".board-item");
+  // console.log(taskList);
+});
+
+taskList.forEach((val, key) => {
+  val.addEventListener("dragstart", (e) => {
+    draggedElement = e.target;
+    // console.log(draggedElement);
+  });
+  val.addEventListener("dragend", (e) => {
+    dropBoard.appendChild(draggedElement);
+    draggedElement = null;
+  });
+  // val.addEventListener("dragover", (e) => {
+  //   e.preventDefault();
+  // });
+});
+
+dropBoardList.forEach((val, key) => {
+  val.addEventListener("dragover", (e) => {
+    if (e.target.className === "board-items") {
+      dropBoard = e.target;
+    }
+    // console.log(e);
+    // console.log(dropBoard);
+  });
 });
